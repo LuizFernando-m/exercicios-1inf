d81c44ecb656ed9059ec91eeff0e4f91a0aa1228
Luis Fernando <luis.soares48@aluno.ce.gov.br>
2026-09-16 17:55:18 +0000

    Atualiza exercícios do projeto

--- 3-bimestre/logica-de-programacao/projetos/projeto1/index.js
+++ 3-bimestre/logica-de-programacao/projetos/projeto1/index.js
@@ -1,4 +1,33 @@
-// CRIE SUA SOLUÇÃO ABAIXO ================
+// const cliente = "Mariana Silva";
+const cliente = "Mariana Silva"
+const cidade = "Fortaleza";
+const produto = "Notebook Pro";
+const categoria = "Notebook";
+const preco = 3500;
+const quantidade = 2;
+const descontoPercentual = 10;
+const valorPago = 7000;
+const subtotal = preco * quantidade;
+const valorDesconto = (subtotal * descontoPercentual) /100;
+const valorFinal = (subtotal - valorDesconto);
+const troco = (valorPago - valorFinal);
+
+console.log(`TECHSTORE SISTEMAS
+  RESUMO DO PEDIDO
+Cliente: ${cliente}
+Cidade: ${cidade}
+Produto: ${produto}
+Categoria: ${categoria}
+Preco: R$ ${preco}
+Quantidade: ${quantidade}
+Subtotal: R$ ${subtotal}
+Desconto: R$ ${valorDesconto}
+Valor Final: R$ ${valorFinal}
+Valor Pago: R$ ${valorPago}
+Troco: R$ ${troco}
+Obrigado pela compra!`)
+
+const resumo = `${cliente} fez o pedido de um ${produto} e recebeu R$ ${troco} de troco tendo R$ ${valorFinal}`
 
 
 
